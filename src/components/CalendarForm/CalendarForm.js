567966f9c0ce691f8eb8cc1753d9f92c754cbda9
Sameer Zaher <sameerzaher@gmail.com>
2024-06-15 13:44:36 +0300

    V1.1.0 event calendar

--- src/components/CalendarForm/CalendarForm.js
+++ src/components/CalendarForm/CalendarForm.js
@@ -1,20 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
 import axios from "axios";
 import { SERVER_URL } from "../../consts";
 import classes from "./CalendarForm.module.scss";
 import { toast } from "react-toastify";
 
-const CalendarForm = ({ token, onEventAdded, spaceId }) => {
+const CalendarForm = ({ token, onEventAdded, spaceId, rooms }) => {
   const [newEventName, setNewEventName] = useState("");
   const [newEventDescription, setNewEventDescription] = useState("");
   const [newEventTime, setNewEventTime] = useState(dayjs());
   const [newEventType, setNewEventType] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [devices, setDevices] = useState([]);
+  const [selectedDevices, setSelectedDevices] = useState({});
+  const [acTemperature, setAcTemperature] = useState(25);
+  const [acMode, setAcMode] = useState('cool');
+  const [acState, setAcState] = useState('on');
+
+  useEffect(() => {
+    const fetchDevices = async () => {
+      if (roomName) {
+        try {
+          const response = await fetch(`${SERVER_URL}/api-device/device/space/${spaceId}/${encodeURIComponent(roomName)}`);
+          if (response.ok) {
+            const data = await response.json();
+            console.log("Fetched devices:", data); // Debug log
+            setDevices(data);
+          } else {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+        } catch (error) {
+          console.error('Failed to fetch devices:', error);
+          toast.error(`Failed to fetch devices. ${error.message}`);
+        }
+      }
+    };
+
+    fetchDevices();
+  }, [roomName, spaceId]);
+
+  const handleDeviceSelectionChange = (device) => {
+    setSelectedDevices(prevState => {
+      if (prevState[device]) {
+        const newState = { ...prevState };
+        delete newState[device];
+        return newState;
+      } else {
+        return { ...prevState, [device]: 'on' };
+      }
+    });
+  };
+
+  const handleRoomChange = (event) => {
+    const selectedRoomName = event.target.value;
+    setRoomName(selectedRoomName);
+    console.log(`Selected room name: ${selectedRoomName}`);
+  };
 
   const handleNewEventSubmit = async (event) => {
     event.preventDefault();
@@ -23,14 +70,16 @@ const CalendarForm = ({ token, onEventAdded, spaceId }) => {
     const newEvent = {
       title: newEventName,
       description: newEventDescription,
-      time: newEventTime.toISOString(), // Ensure to use "time" instead of "Time"
+      time: newEventTime.toISOString(),
       eventType: newEventType,
-      space_id: spaceId // Include the spaceId in the new event data
+      space_id: spaceId,
+      roomName: roomName,
+      roomDevices: Object.keys(selectedDevices).filter(deviceId => selectedDevices[deviceId])
     };
 
     try {
       await axios.post(`${SERVER_URL}/api-calendar/add-events`, newEvent, {
-        headers: { Authorization: `Bearer ${token}` }
+        headers: { Authorization: `Bearer ${token}` },
       });
       toast.info("Calendar Event Added successfully!");
       onEventAdded();
@@ -38,6 +87,8 @@ const CalendarForm = ({ token, onEventAdded, spaceId }) => {
       setNewEventDescription("");
       setNewEventTime(dayjs());
       setNewEventType("");
+      setRoomName("");
+      setSelectedDevices({});
     } catch (error) {
       console.error("Error adding new event:", error.response ? error.response.data : error);
       toast.error("Failed to add event.");
@@ -62,21 +113,58 @@ const CalendarForm = ({ token, onEventAdded, spaceId }) => {
         margin="normal"
       />
       <TextField
-       
+        select
+        label="Room Name"
+        value={roomName}
+        onChange={handleRoomChange}
+        required
+        fullWidth
+        margin="normal"
+      >
+        <MenuItem value="">
+          <em>Select Room</em>
+        </MenuItem>
+        {rooms.map((room) => (
+          <MenuItem key={room._id} value={room.name}>
+            {room.name}
+          </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        select
+        label="Event Type"
         value={newEventType}
         onChange={(e) => setNewEventType(e.target.value)}
         required
-        select
         fullWidth
         margin="normal"
-        SelectProps={{ native: true }}
         className={classes.EventTypeField}
       >
-        <option value="">Select Event Type</option>
-        <option value="holiday">Holiday</option>
-        <option value="weekend">Weekend</option>
-        <option value="lecture">Lecture</option>
+        <MenuItem value="">
+          <em>Select Event Type</em>
+        </MenuItem>
+        <MenuItem value="holiday">Holiday</MenuItem>
+        <MenuItem value="weekend">Weekend</MenuItem>
+        <MenuItem value="lecture">Lecture</MenuItem>
       </TextField>
+      <h3 className={classes.sectionTitle}>Devices</h3>
+      <div className={classes.deviceRow}>
+
+        {devices.map((device, index) => (
+          <div key={index} className={classes.deviceCheckbox}>
+            <label htmlFor={`device_${device}`} className={classes.deviceLabel}>
+              <input
+                type="checkbox"
+                id={`device_${device}`}
+                name={`device_${device}`}
+                value={device}
+                onChange={() => handleDeviceSelectionChange(device)}
+              />
+              {device}
+            </label>
+          </div>
+        ))}
+      </div>
       <div className={classes.DateTimeContainer}>
         <div className={classes.DateTimeLabel}>Time</div>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -95,7 +183,11 @@ const CalendarForm = ({ token, onEventAdded, spaceId }) => {
 CalendarForm.propTypes = {
   token: PropTypes.string.isRequired,
   onEventAdded: PropTypes.func.isRequired,
-  spaceId: PropTypes.string.isRequired, // Add spaceId to prop types
+  spaceId: PropTypes.string.isRequired,
+  rooms: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
 export default CalendarForm;
--- src/containers/CalendarDashboard/CalendarDashboard.js
+++ src/containers/CalendarDashboard/CalendarDashboard.js
@@ -10,12 +10,11 @@ import { SERVER_URL } from "../../consts";
 import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import TextField from "@mui/material/TextField";
 import { toast } from "react-toastify";
 import RulesModal from "../../components/RulesModal/RulesModal";
 import { useSpace } from "../../contexts/SpaceContext";
-import {jwtDecode} from "jwt-decode";
+import CalendarForm from "../../components/CalendarForm/CalendarForm";
 
 const localizer = momentLocalizer(moment);
 
@@ -26,30 +25,35 @@ const CalendarDashboard = ({ token }) => {
   const [search, setSearch] = useState("");
   const [filteredEvents, setFilteredEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
-  const [tokenError, setTokenError] = useState(null);
+  const [rooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [newEventName, setNewEventName] = useState("");
-  const [newEventDescription, setNewEventDescription] = useState("");
-  const [newEventTime, setNewEventTime] = useState(dayjs());
-  const [newEventType, setNewEventType] = useState("");
-
-  const checkToken = (token) => {
-    try {
-      const decodedToken = jwtDecode(token);
-      console.log('Decoded Token:', decodedToken);
-      return decodedToken;
-    } catch (error) {
-      console.error('Invalid token:', error);
-      setTokenError('Invalid token');
-      return null;
+  useEffect(() => {
+    if (!spaceId) {
+      console.error("spaceId is null");
+      return;
     }
-  };
+
+    const fetchRooms = async () => {
+      try {
+        const response = await axios.get(`${SERVER_URL}/api-room/rooms/space/${spaceId}`);
+        if (response.status === 200) {
+          setRooms(response.data);
+        } else {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error('Failed to fetch rooms:', error);
+        toast.error(`Failed to fetch rooms. ${error.message}`);
+      }
+    };
+
+    fetchRooms();
+  }, [spaceId]);
 
   useEffect(() => {
     if (spaceId) {
       fetchEvents(spaceId);
-    } else {
-      console.error("spaceId is null");
     }
   }, [spaceId, token]);
 
@@ -61,9 +65,11 @@ const CalendarDashboard = ({ token }) => {
       });
       toast.info("Events fetched successfully!");
       setEvents(response.data);
+      setLoading(false);
     } catch (error) {
       console.error("Failed to fetch events:", error);
       toast.error("Failed to fetch events.");
+      setLoading(false);
     }
   };
 
@@ -81,52 +87,6 @@ const CalendarDashboard = ({ token }) => {
     }
   };
 
-  const handleNewEventSubmit = async (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    const newEvent = {
-      title: newEventName,
-      description: newEventDescription,
-      time: newEventTime.toISOString(), // Ensure this is "time"
-      eventType: newEventType,
-      space_id: spaceId
-    };
-
-    try {
-      await axios.post(`${SERVER_URL}/api-calendar/add-events`, newEvent, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
-      toast.info("Calendar Event Added successfully!");
-      fetchEvents(spaceId);
-      setNewEventName("");
-      setNewEventDescription("");
-      setNewEventTime(dayjs());
-      setNewEventType("");
-      setOpenAddEventModal(false);
-    } catch (error) {
-      console.error("Error adding new event:", error.response ? error.response.data : error);
-    }
-  };
-
-  const handleDeleteEvent = async (eventId) => {
-    try {
-      const response = await axios.delete(`${SERVER_URL}/api-calendar/delete-event/${eventId}`, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
-
-      if (response.status === 204) {
-        toast.info("Event deleted successfully!");
-        setEvents(events.filter(event => event._id !== eventId));
-      } else {
-        throw new Error("Failed to delete event");
-      }
-    } catch (error) {
-      console.error("Error deleting event:", error.response ? error.response.data : error);
-      toast.error("Failed to delete event.");
-    }
-  };
-
   const handleOpenAddEventModal = () => {
     setOpenAddEventModal(true);
   };
@@ -137,7 +97,6 @@ const CalendarDashboard = ({ token }) => {
 
   return (
     <div className={classes.CalendarDashboard}>
-
       <div className={classes.SearchContainer}>
         <input
           type="text"
@@ -146,7 +105,7 @@ const CalendarDashboard = ({ token }) => {
           placeholder="Search for an event..."
           className={classes.SearchInput}
         />
-            <button onClick={handleOpenAddEventModal} className={classes.addButton}>Add Event</button>
+        <button onClick={handleOpenAddEventModal} className={classes.addButton}>Add Event</button>
 
         {filteredEvents.length > 0 && (
           <div className={classes.FilteredEvents}>
@@ -162,70 +121,37 @@ const CalendarDashboard = ({ token }) => {
           </div>
         )}
       </div>
-      
+
       <RulesModal show={openAddEventModal} onCloseModal={handleCloseAddEventModal} title="Add Calendar Event">
-        <form onSubmit={handleNewEventSubmit} className={classes.NewEventForm}>
-          <TextField
-            label="Event Name"
-            value={newEventName}
-            onChange={(e) => setNewEventName(e.target.value)}
-            required
-            fullWidth
-            margin="normal"
+        {spaceId ? (
+          <CalendarForm
+            token={token}
+            spaceId={spaceId}
+            onEventAdded={() => fetchEvents(spaceId)}
+            rooms={rooms}
           />
-          <TextField
-            label="Event Description"
-            value={newEventDescription}
-            onChange={(e) => setNewEventDescription(e.target.value)}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-           
-            value={newEventType}
-            onChange={(e) => setNewEventType(e.target.value)}
-            required
-            select
-            fullWidth
-            margin="normal"
-            SelectProps={{ native: true }}
-            className={classes.EventTypeField}
-          >
-            <option value="">Select Event Type</option>
-            <option value="holiday">Holiday</option>
-            <option value="weekend">Weekend</option>
-            <option value="lecture">Lecture</option>
-          </TextField>
-          <div className={classes.DateTimeContainer}>
-            <div className={classes.DateTimeLabel}>Time</div>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DateTimePicker
-                value={newEventTime}
-                onChange={(newValue) => setNewEventTime(newValue)}
-                renderInput={(params) => <TextField {...params} fullWidth />}
-              />
-            </LocalizationProvider>
-          </div>
-          <div className={classes.DatePostion}>
-             <button type="submit" className={classes.addEventButton}>Add Event</button>
-          </div>
-        </form>
+        ) : (
+          <p>Loading...</p>
+        )}
       </RulesModal>
 
-      <Calendar
-        localizer={localizer}
-        events={events.map(event => ({
-          ...event,
-          start: new Date(event.time), // Ensure to use "time" instead of "start"
-          end: new Date(event.time) // Ensure to use "time" instead of "end"
-        }))}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        onSelectEvent={(event) => setSelectedEvent(event._id)}
-      />
+      {loading ? (
+        <p>Loading events...</p>
+      ) : (
+        <Calendar
+          localizer={localizer}
+          events={events.map(event => ({
+            ...event,
+            start: new Date(event.time),
+            end: new Date(event.time)
+          }))}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          onSelectEvent={(event) => setSelectedEvent(event._id)}
+        />
+      )}
 
-    
       {spaceId && (
         <EventsTable
           events={filteredEvents.length > 0 ? filteredEvents : events}
@@ -233,7 +159,7 @@ const CalendarDashboard = ({ token }) => {
           selectedEvent={selectedEvent}
           token={token}
           spaceId={spaceId}
-          fetchEvents={fetchEvents} // Pass the fetchEvents function to refresh the events list
+          fetchEvents={fetchEvents}
         />
       )}
     </div>
@@ -243,7 +169,5 @@ const CalendarDashboard = ({ token }) => {
 CalendarDashboard.propTypes = {
   token: PropTypes.string.isRequired,
 };
-CalendarDashboard.defaultProps = {
-    token: "", // Default to an empty string
-};
+
 export default CalendarDashboard;
